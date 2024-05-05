cdf05fa7e01caf7ac18f80ac3c001ecd49da8add
Ankit <anisingh565072@gmail.com>
2024-05-05 19:16:55 +0530

    Initial commit

--- Day1.java
+++ Day1.java
@@ -2,8 +2,19 @@ public class Day1 {
 
     public static void main(String[] args ){
 
-        System.out.println(Problem_no1("Hello world"));
+        System.out.println(Problem_no2());
 
+    }
+
+    public static String Problem_no2(){
+        String s = "My name is Ankit";
+        String ans = "";
+        for (int i = 0 ; i <= s.length() -1; i++){
+            if(s.charAt(i) != ' '){
+                ans = ans + s.charAt(i);
+            }
+        }
+        return ans;
     }
     public static String  Problem_no1(String s ){
         String reverse = "";
