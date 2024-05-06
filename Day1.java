babc2c7956736fa750a2646e19a4f6538e24bb20
Ankit <anisingh565072@gmail.com>
2024-05-06 17:52:17 +0530

    Third Commit

--- Day1.java
+++ Day1.java
@@ -2,9 +2,43 @@ public class Day1 {
 
     public static void main(String[] args ){
 
-        System.out.println(Problem_no2());
+//        System.out.println(Problem_no2());
+        Day1 obj = new Day1();
+//        obj.Problem_no3("Ankit");
+        System.out.println(obj.Problem_no4("Ankit", "Ankit"));
 
     }
+
+
+    public boolean Problem_no4(String s1 , String s2){
+        int count = 0;
+//        In this problem check whether two string are Identical Or Not
+        if(s1.length() != s2.length()){
+            return false;
+        }
+        else{
+            for(int i = 0 ; i < s1.length() -1; i++){
+                if(s1.charAt(i) == s2.charAt(i)){
+                    count++;
+                    break;
+                }
+                else{
+                    count = count+0;
+                }
+            }
+
+        }
+        if(count == s1.length()){
+            return true;
+        }
+        return false;
+
+
+
+
+    }
+
+
 
    public String Problem_no3(String s ){
         String ans = "";
