349f8c129a61a63b64ae721d6994beec47453a18
Ankit <anisingh565072@gmail.com>
2024-05-08 20:13:00 +0530

    Day 2 first commit

---
+++ Day2.java
@@ -0,0 +1,21 @@
+public class Day2 {
+
+    public static void main(String[] args) {
+        Day2 obj = new Day2();
+        obj.Problem_no_1("ankit", "Ankit");
+
+    }
+    public void Problem_no_1(String s1 , String s2){
+        if(s1.equals(s2)){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+
+    }
+
+
+
+
+}
